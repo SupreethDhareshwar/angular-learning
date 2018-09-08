9274c95029cd9f3c6acffa5ae686544444dd07e1
Supreeth D <supreeth.d@gieom.com>
2018-09-08 12:49:28 +0530

    adding sidebar , header and home compoent  simple design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    DetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
