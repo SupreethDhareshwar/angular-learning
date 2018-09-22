4eaf61cd67fa584a0661cc0527e2e8c3fa4fa401
Supreeth D <supreeth.d@gieom.com>
2018-09-22 15:05:22 +0530

    Defining Common Header Component for all routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import {
+  MatToolbarModule
+} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+  ]
+})
+export class DemoMaterialModule {}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    DemoMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
